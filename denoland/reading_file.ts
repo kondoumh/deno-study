da77cebcd62045fbaadb1ec1e0e2c7ff96d3e105
kondoumh <kondoumh@gmail.com>
2020-09-26 14:49:13 +0900

    first commit

---
+++ denoland/reading_file.ts
@@ -0,0 +1,8 @@
+// deno run --allow-read reading_file.ts {path | url} to file
+
+const filenames = Deno.args;
+for (const filename of filenames) {
+  const file = await Deno.open(filename);
+  await Deno.copy(file, Deno.stdout);
+  file.close();
+}
