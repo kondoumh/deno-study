b77a6e496cff35df2270f3075f1d24d08ed31954
kondoumh <kondoumh@gmail.com>
2022-08-13 17:21:53 +0900

    add std/http lib version

---
+++ denoland/examples/7-http-web-server/webserver2.ts
@@ -0,0 +1,15 @@
+// deno run --allow-net webserver2.ts
+import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
+
+const port = 8080;
+
+const handler = (request: Request): Response => {
+  const body = `Your user-agent is \n\n${
+    request.headers.get("user-agent") ?? "Unknown"
+  }`;
+
+  return new Response(body, { status: 200 });
+};
+
+console.log(`HTTP webserver running. Access it at: http://localhost:8080/`)
+await serve(handler, { port });
