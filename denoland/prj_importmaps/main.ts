b06badffcdbd627bbdb496ed44018ba88b814189
kondoumh <kondoumh@gmail.com>
2022-09-21 17:06:54 +0900

    add import maps

--- denoland/prj_importmaps/main.ts
+++ denoland/prj_importmaps/main.ts
@@ -1,3 +1,6 @@
-import { red } from "fmt/colors.ts"
+import { red } from "fmt/colors.ts";
+import lodash from "lodash";
+import { add } from "add";
 
 console.log(red("hello world"));
+console.log(add(2, 3));
