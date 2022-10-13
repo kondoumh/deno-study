d04fa37e122976c3be4d3166b616646b272c55bb
kondoumh <kondoumh@gmail.com>
2022-10-13 12:17:47 +0900

    Add deno deploy ssr example

---
+++ deno-deploy/jsx/date.tsx
@@ -0,0 +1,20 @@
+/** @jsx h */
+// deno run --allow-net=:8000 date.tsx
+import {serve} from "https://deno.land/std@0.140.0/http/server.ts";
+import { h } from "https://esm.sh/preact@10.5.15";
+import { renderToString } from "https://esm.sh/preact-render-to-string@5.1.19?deps=preact@10.5.15";
+
+function handler(_req: Request): Response {
+  const page = (
+    <div>
+      <h1>Current time</h1>
+      <p>{new Date().toLocaleString()}</p>
+    </div>
+  );
+  const html = renderToString(page);
+  return new Response(html, {
+    headers: { "content-type": "text/html; charset=utf-8" },
+  });
+}
+
+serve(handler);
