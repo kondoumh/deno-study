ab63883fd1e5364f25444436c343e3af53ce5079
kondoumh <kondoumh@gmail.com>
2022-10-13 14:58:24 +0900

    add deno-dom example

---
+++ denoland/jsx-dom/deno-dom.tsx
@@ -0,0 +1,29 @@
+import {serve} from "https://deno.land/std@0.140.0/http/server.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
+
+function handler(_req: Request): Response {
+  const document = new DOMParser().parseFromString(
+    `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>Hello from Deno</title>
+      </head>
+      <body>
+        <h1>Hello from Deno</h1>
+        <form>
+          <input name="user">
+          <button>
+            Submit
+          </button>
+        </form>
+      </body>
+    </html>`,
+    "text/html",
+  );
+  return new Response(document, {
+    headers: { "content-type": "text/html; charset=utf-8" },
+  });
+}
+
+serve(handler);
