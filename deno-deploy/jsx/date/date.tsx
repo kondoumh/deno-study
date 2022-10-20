35ba73989a7ffeb436ab6af0b1a2079023c79d1c
kondoumh <kondoumh@gmail.com>
2022-10-21 08:45:31 +0900

    Add json files

--- deno-deploy/jsx/date/date.tsx
+++ deno-deploy/jsx/date/date.tsx
@@ -1,14 +1,13 @@
 /** @jsx h */
-// deno run --allow-net=:8000 date.tsx
-import {serve} from "https://deno.land/std@0.160.0/http/server.ts";
-import { h } from "https://esm.sh/preact@10.11.2";
-import { renderToString } from "https://esm.sh/preact-render-to-string@5.2.5";
+import { h } from "preact";
+import { renderToString } from "preact-render-to-string";
+import { serve } from "serve";
 
 function handler(_req: Request): Response {
   const page = (
     <div>
       <h1>Current time</h1>
-      <p>{new Date().toLocaleString()}</p>
+      <p>{ new Date().toLocaleString() }</p>
     </div>
   );
   const html = renderToString(page);
