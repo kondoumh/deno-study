eb7a614612d0945b77c3b53613f640e7b63c9e1a
kondoumh <kondoumh@gmail.com>
2022-11-22 01:19:33 +0900

    add filter

--- deno-deploy/serve-json/mod.ts
+++ deno-deploy/serve-json/mod.ts
@@ -2,6 +2,18 @@ import { serve } from "https://deno.land/std@0.165.0/http/server.ts";
 import { serveFile } from "https://deno.land/std@0.165.0/http/file_server.ts";
 
 async function handleRequest(request: Request): Promise<Response> {
+  const { pathname } = new URL(request.url);
+  if (pathname.startsWith("/todo")) {
+    const text = await Deno.readTextFile("./todo.json");
+    const data = JSON.parse(text);
+    const todos = data.todos.filter(item => item.done === false);
+    data.todos = todos;
+    return new Response(JSON.stringify(data, null, 2), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  }
   return await serveFile(request, `${Deno.cwd()}/todo.json`);
 }
 
