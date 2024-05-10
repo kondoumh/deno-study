06a7183f2b3d3cbeabc548fcd403752dc26f13ec
Masahiro Kondo <kondoumh@gmail.com>
2024-05-10 10:14:04 +0900

    add jsr package source

---
+++ adder/main.ts
@@ -0,0 +1,10 @@
+// main.ts
+/** Add two numbers
+ *
+ * @param a The first number
+ * @param b The second number
+ * @returns The sum of the two numbers
+ */
+export function add(a: number, b: number): number {
+  return a + b;
+}
